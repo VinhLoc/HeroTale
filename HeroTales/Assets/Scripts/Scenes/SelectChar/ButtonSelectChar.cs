b698862e3ea4f16c71a9f4f741b1f6d9f64148ed
nguyencb <nguyen@playfury.com>
2014-06-21 21:29:20 +0700

    - update select char scene

--- HeroTales/Assets/Scripts/Scenes/SelectChar/ButtonSelectChar.cs
+++ HeroTales/Assets/Scripts/Scenes/SelectChar/ButtonSelectChar.cs
@@ -6,9 +6,16 @@ public class ButtonSelectChar : MonoBehaviour
 	public InfoStats.CLASS_TYPE buttonClass;
 	public SelectCharControl mainControl;
 
+	private tk2dUIToggleButton toggle;
+
+	void Awake()
+	{
+		toggle = GetComponent<tk2dUIToggleButton> ();
+	}
+
 	public void OnClick()
 	{
-		mainControl.SelectChar (buttonClass);
+		mainControl.SelectChar (buttonClass, toggle);
 	}
 }
 
